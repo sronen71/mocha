e26863d106e7848447361e6aa87d760904e416b2
Shai Ronen <sronen71@yahoo.com>
2015-04-26 14:03:20 -0700

    update

--- examples/average.py
+++ examples/average.py
@@ -1,30 +1,34 @@
 import csv
+import numpy as np
 
-FILES=["submit-inet10.csv","submit_inet10_AR.csv","submit-inet11.csv","submit_inet11_AR.csv","submit_deeper.csv"]
-SUBMIT="ensemble-10-10AR-11-11AR-deeper.csv"
+FILES=["ensemble_10H_10AR_11H_11AR_DH_DAR.csv","submit-deeper-B.csv"]
+weights=[6.0,1.0]
+SUBMIT="ensemble-10H-10AR-11H-11AR-DH_DAR_DB.csv"
 PATH="plankton/"
 average={}
 count=0
-for f in FILES:
+for ifile,f in enumerate(FILES):
     with open(PATH+f,'rb') as csvfile:
         count+=1
-        print count
+        print "count: ",count
         rows=csv.reader(csvfile)
         header=next(rows, None)
         encode=header[1:]
         for k,row in enumerate(rows): 
-            predictions=[float(x) for x in row[1:]]
+            predictions=np.array([float(x) for x in row[1:]])
+            #print "sum: ",np.sum(predictions)
             name=row[0]
             if count==1:
-                average[name]=predictions  
+                #print "ifile",ifile
+                average[name]=predictions*weights[ifile]
             else:
-                average[name]=[predictions[i]+average[name][i] 
+                average[name]=[predictions[i]+average[name][i]*weights[ifile] 
                         for i in range(0,len(predictions))]
 print len(average)
 with open(PATH+SUBMIT,'w') as csvfile:
     writer=csv.writer(csvfile)
     writer.writerow(header)
     for key  in average:
-        average[key]=[average[key][k]/len(FILES) for k in range(0,len(average[key]))]
+        average[key]=[average[key][k]/sum(weights) for k in range(0,len(average[key]))]
         writer.writerow([key]+average[key])
     
---
+++ examples/plankton/plankton.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import lmdb
+import caffe
+import matplotlib.cm as cm
+import sklearn.metrics as metrics
+import sys
+import csv
+import cv2
+
+caffe_root = '../'
+sys.path.insert(0,caffe_root+'python')
+#SUBMIT=True
+SUBMIT=False
+
+#oversample=True
+#supersample=True
+
+oversample=False
+supersample=False
+
+tsize=64 # 64
+
+
+if SUBMIT:
+    TEST_DB="plankton/plankton_test_lmdb"
+    SUBMIT_FILE="plankton/submit.csv"
+else:
+    TEST_DB='plankton/plankton_val_lmdb'
+
+ENCODE_FILE="/home/ubuntu/mocha/data/plankton/encode.txt"
+MODEL_FILE='plankton/inet_deploy9.prototxt'
+PRETRAINED='plankton/inet9.caffemodel'
+
+
+
+print "Try to create net..."
+net = caffe.Classifier(MODEL_FILE, PRETRAINED,
+#	mean=np.load('plankton/mean.npy'),
+        image_dims=(tsize, tsize))
+print "Created net"
+net.set_phase_test()
+net.set_mode_gpu()
+
+env=lmdb.open(TEST_DB)
+datum = caffe.proto.caffe_pb2.Datum()
+
+def write_submit(filename,keys,predictions,labels=[]):
+    import csv
+    with open(ENCODE_FILE, 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        encode=[]
+        for row in reader:
+            encode.append(row[1])
+    with open(filename,'w') as csvfile:
+        writer=csv.writer(csvfile)
+        writer.writerow(["image"]+encode)
+        for i,predict in enumerate(predictions):
+            name=(keys[i].split("."))[0]+".jpg"
+            name=''.join(name.split('_')[1:])
+            row=[name]+predict.tolist()
+            if labels:
+                label=[str(labels[i])]
+                row=row+label
+            writer.writerow(row) 
+
+
+def chunks(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+
+
+def getimages(datum,angle=0,rescale=1.0):
+    with env.begin() as txn:
+        cursor = txn.cursor()           # Cursor on main database.
+        cursor.first()
+        images=[]
+        labels=[]
+        keys=[]
+        
+        for key,value in cursor:
+            datum.ParseFromString(value)
+            arr=caffe.io.datum_to_array(datum)  # [c,h,w]
+            arr=arr.reshape(arr.shape[1],arr.shape[2],arr.shape[0]) # [h,w,c]
+            h=arr.shape[0]
+            actual_size=datum.actual_size
+            if actual_size>arr.shape[0]:
+                actual_size=arr.shape[0]    
+            #inter=cv2.cv.CV_INTER_LINEAR
+            inter=cv2.cv.CV_INTER_CUBIC + cv2.cv.CV_WARP_FILL_OUTLIERS
+
+            f=float(tsize)/actual_size*rescale
+            img=np.squeeze(arr)
+            M=cv2.getRotationMatrix2D((h/2,h/2),angle,f)
+            M[0][2]-=(h-tsize)/2
+            M[1][2]-=(h-tsize)/2
+            img=img.astype(np.float32)
+            img1 = cv2.warpAffine(img,M,(tsize,tsize),flags=inter,borderMode=cv2.BORDER_CONSTANT)
+            #img1=cv2.resize(img,(0,0),fx=f,fy=f,interpolation=inter)
+            #l=(img1.shape[0]-tsize)/2
+            #img1=img1[l:l+tsize,l:l+tsize]
+            img1=np.clip(img1,-1000,255)
+            img1=np.floor(img1)
+
+            #print np.min(img),np.max(img),np.min(img1),np.max(img1)
+            arr=img1[:,:,None]
+
+
+            images.append(arr.astype(np.float32)) # python wrapper needs float32, e.g for resize
+            labels.append(datum.label)
+            keys.append(key)
+            #plt.imshow(img,cmap=cm.Greys_r)
+            #plt.show()
+            #plt.imshow(img1,cmap=cm.Greys_r)
+            #plt.show()
+        print "#images: ", len(images)    
+    return images,labels,keys
+
+
+if supersample:
+    angles=range(0,360,45)
+    rescales=[0.95,1.0,1.05]
+#  rescales=[1.0]
+else:
+    angles=[0]
+    rescales=[1.0]
+
+pre=[]
+for angle in angles:
+    for rescale in rescales:
+        pre.append((angle,rescale))
+
+predictions=np.zeros(1)
+for config in pre:
+    angle=config[0]
+    rescale=config[1]
+    print "angle,rescale:", angle,rescale
+    images,labels,keys=getimages(datum,angle,rescale)
+    if angle is 0:
+        predcitions=np.zeros(len(images))
+    predictions1=[]
+    count=0
+    for chunk in chunks(images,len(images)/10): 
+        count+=1
+        print count
+        predictions_chunk = net.predict(chunk,oversample=oversample)  
+        predictions1.extend(predictions_chunk)
+    predictions=predictions+predictions1
+    
+predictions=predictions/len(pre)
+
+
+if SUBMIT:
+    write_submit(SUBMIT_FILE,keys,predictions)
+else:
+    predictions=np.array(predictions)
+    nlabels=np.zeros(predictions.shape)
+    probs=np.zeros(predictions.shape[0])
+    for i,label in enumerate(labels):
+        nlabels[i,label]=1
+        probs[i]=predictions[i,label]
+    score=-np.mean(np.log(probs))
+    loss=metrics.log_loss(nlabels,predictions,eps=1e-15)
+    print "log loss score:", score
+
+    print "regularized score:",loss
+    write_submit('val.csv',keys,predictions,labels)
